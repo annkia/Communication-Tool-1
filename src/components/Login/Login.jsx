9ec8317d9ebf88822713fa1473b2eb89a1417a2d
Seweryn Wadowski <wadowski17@gmail.com>
2019-03-21 16:35:35 +0100

    adde google login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import style from "./Login.module.scss";
+import GoogleLogin from "react-google-login";
 
 export default class Login extends React.Component {
   render() {
+    const responseGoogle = response => {
+      console.log(response);
+    };
+
     return (
       <React.Fragment>
         <div className={style.login}>
           <p>Siemano</p>
+          <GoogleLogin
+            className={style.login__googleButton}
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+          />
         </div>
       </React.Fragment>
     );
