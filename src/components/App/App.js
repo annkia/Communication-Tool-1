65f87ffb8337d627a6aee58eb7956a12a0a09b5a
Seweryn Wadowski <wadowski17@gmail.com>
2019-03-21 13:20:30 +0100

    Initializing default react project

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import style from './App.module.scss'
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <p className={style.App}>Siemano</p>
+        <Route to="" component="" />
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
