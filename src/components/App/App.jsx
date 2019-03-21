3cac213664412490a58c3ecfaba00b32576c2bb7
Seweryn Wadowski <wadowski17@gmail.com>
2019-03-21 14:36:07 +0100

    Added ESlint configuration

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import style from './App.module.scss'
-
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import style from "./App.module.scss";
 
 class App extends Component {
   render() {
