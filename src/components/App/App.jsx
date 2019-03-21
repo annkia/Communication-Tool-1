af35800c6ef069f47234b4a69f48d253eb68d2a2
Seweryn Wadowski <wadowski17@gmail.com>
2019-03-21 16:13:28 +0100

    init Login component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import style from "./App.module.scss";
 
+import Login from "../Login/Login";
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <p className={style.App}>Siemano</p>
-        <Route to="" component="" />
+        <Route to="/" component={Login} />
       </BrowserRouter>
     );
   }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Login.module.scss";
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className={style.login}>
+          <p>Siemano</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
