c99ec35b344c8c69e5df704cde7e1ae93f2a20cf
doman89 <mateusz.domanski.1989@gmail.com>
2019-04-03 17:09:04 +0200

    Changed method sortByPublishedDate on getter userPostsSorted.

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 
 const styles = {
-    Image: {
-        margin: 'auto',
-        marginTop: 20,
-        display: 'block',
-        width: 250,
-        height: 250,
-    }
-}
+  Image: {
+    margin: "auto",
+    marginTop: 20,
+    display: "block",
+    width: 250,
+    height: 250
+  }
+};
 
 export class Photo extends React.Component {
-    render() {
-        return (
-            <Avatar alt="User avatar photo" src={this.props.profilePhoto} style={styles.Image} />
-        );
-    }
+  render() {
+    return (
+      <Avatar
+        alt="User avatar photo"
+        src={this.props.profilePhoto}
+        style={styles.Image}
+      />
+    );
+  }
 }
