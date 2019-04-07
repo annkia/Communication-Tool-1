b3f4e31887fca6d5e24bfaf3f75af00cf67bb6a2
doman89 <mateusz.domanski.1989@gmail.com>
2019-04-07 22:03:13 +0200

    Made code some shorter

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -5,28 +5,20 @@ export const FETCH_POSTS = "Fetch_Posts";
 export const EDIT_POST = "Edit_Post";
 export const REMOVE_POST = "Remove_Post";
 
-export const addPost = ({ userPost, image }) => {
-  return dispatch => {
-    const formData = new FormData();
-    formData.append("photo", image);
-    formData.append("post", userPost);
-    return Axios.createPost(formData)
-      .then(response => {
-        dispatch(addPostSuccess(response));
-      })
-      .catch(error => {
-        throw error;
-      });
-  };
+export const addPost = ({ userPost, image }) => dispatch => {
+  const formData = new FormData();
+  formData.append("photo", image);
+  formData.append("post", userPost);
+  return Axios.createPost(formData)
+    .then(response => {
+      dispatch(addPostSuccess(response));
+    })
+    .catch(error => {
+      throw error;
+    });
 };
 
-const addPostSuccess = ({
-  Id,
-  Title,
-  Text,
-  ThumbnailPhoto,
-  PublishDate
-}) => ({
+const addPostSuccess = ({ Id, Title, Text, ThumbnailPhoto, PublishDate }) => ({
   type: ADD_POST,
   payload: {
     Id,
@@ -37,30 +29,24 @@ const addPostSuccess = ({
   }
 });
 
-export const fetchPosts = () => {
-  return dispatch => {
-    return Axios.getPosts()
-      .then(response => {
-        dispatch(fetchPostsSuccess(response));
-      })
-      .catch(error => {
-        throw error;
-      });
-  };
-};
+export const fetchPosts = () => dispatch =>
+  Axios.getPosts()
+    .then(response => {
+      dispatch(fetchPostsSuccess(response));
+    })
+    .catch(error => {
+      throw error;
+    });
 
 const fetchPostsSuccess = postsArray => ({
   type: FETCH_POSTS,
   payload: postsArray
 });
 
-export const editPost = post => {
-  return dispatch => {
-    return Axios.updatePost(post.Id, post).then(response => {
-      dispatch(editPostSuccess(response));
-    });
-  };
-};
+export const editPost = post => dispatch =>
+  Axios.updatePost(post.Id, post).then(response => {
+    dispatch(editPostSuccess(response));
+  });
 
 const editPostSuccess = ({ Id, Title, Text, ThumbnailPhoto }) => ({
   type: EDIT_POST,
@@ -72,17 +58,14 @@ const editPostSuccess = ({ Id, Title, Text, ThumbnailPhoto }) => ({
   }
 });
 
-export const removePost = id => {
-  return dispatch => {
-    return Axios.deletePost(id)
-      .then(response => {
-        dispatch(removePostSuccess(id));
-      })
-      .catch(error => {
-        throw error;
-      });
-  };
-};
+export const removePost = id => dispatch =>
+  Axios.deletePost(id)
+    .then(response => {
+      dispatch(removePostSuccess(id));
+    })
+    .catch(error => {
+      throw error;
+    });
 
 const removePostSuccess = id => ({
   type: REMOVE_POST,
